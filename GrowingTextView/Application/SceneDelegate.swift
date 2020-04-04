03f8640d57438f9e80425498cd2be5ad39678714
Maciej Gomółka <mgomolka92@gmail.com>
2020-04-04 13:24:01 +0200

    Refactor project

---
+++ GrowingTextView/Application/SceneDelegate.swift
@@ -0,0 +1,17 @@
+import UIKit
+import SwiftUI
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+  var window: UIWindow?
+
+  func scene(_ scene: UIScene,
+             willConnectTo session: UISceneSession,
+             options connectionOptions: UIScene.ConnectionOptions) {
+    guard let windowScene = scene as? UIWindowScene else { return }
+    window = UIWindow(windowScene: windowScene)
+    window?.rootViewController = UIHostingController(rootView: ContentView())
+    window?.makeKeyAndVisible()
+  }
+
+}
