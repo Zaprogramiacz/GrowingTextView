e0e177821c82ade8a6b78ebd5acaa1c5a338a42e
Maciej Gomółka <mgomolka92@gmail.com>
2020-04-04 14:19:31 +0200

    Pass text binding to text view wrapper, change text font

--- GrowingTextView/Views/ContentView.swift
+++ GrowingTextView/Views/ContentView.swift
@@ -1,9 +1,12 @@
 import SwiftUI
 
 struct ContentView: View {
+  @State var text: String?
+
   var body: some View {
     VStack {
-      TextViewWrapper()
+      Text(text ?? "")
+      TextViewWrapper(text: $text)
     }
   }
 }
--- GrowingTextView/Views/TextViewWrapper.swift
+++ GrowingTextView/Views/TextViewWrapper.swift
@@ -1,17 +1,45 @@
 import SwiftUI
 
 struct TextViewWrapper: UIViewRepresentable {
+
+  init(text: Binding<String?>) {
+    self._text = text
+  }
+
+  @Binding var text: String?
+
   func makeUIView(context: Context) -> UITextView {
-    UITextView()
+    let textView = UITextView()
+    textView.delegate = context.coordinator
+    textView.font = .systemFont(ofSize: 16)
+    return textView
+  }
+
+  func makeCoordinator() -> Coordinator {
+    Coordinator(text: $text)
   }
 
   func updateUIView(_ uiView: UITextView, context: Context) {}
+
+  class Coordinator: NSObject, UITextViewDelegate {
+    init(text: Binding<String?>) {
+      self._text = text
+    }
+
+    @Binding var text: String?
+
+    func textViewDidChange(_ textView: UITextView) {
+      text = textView.text
+    }
+  }
 }
 
 #if DEBUG
 struct TextViewWrapper_Previews: PreviewProvider {
+  @State static var text: String?
+
   static var previews: some View {
-    TextViewWrapper()
+    TextViewWrapper(text: $text)
   }
 }
 #endif
